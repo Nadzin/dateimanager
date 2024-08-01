edcd3176f169e47f18c55839532c26d7e18f03b7
Kai Nadezkin <kai.nadezkin@alkyone-consulting.com>
2024-08-01 09:55:43 +0200

    Showing Android Documents Directory as default on the homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -12,11 +12,9 @@
       <div class="button-container">
             <ion-button fill="solid">+</ion-button>
       </div>
-      <ion-list>
-        <ion-item>
-
-        </ion-item>
-      </ion-list>
+      <div class="list">
+        <ion-list id="file-list"></ion-list>
+      </div>
     </ion-content>
     <ion-footer>
     </ion-footer>
@@ -25,6 +23,37 @@
 
 <script setup lang="ts">
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+
+//Liste füllen
+import { Filesystem, Directory } from '@capacitor/filesystem';
+
+document.addEventListener('DOMContentLoaded', async () => {
+  await loadFiles();
+});
+
+async function loadFiles() {
+  try {
+    const result = await Filesystem.readdir({
+      directory: Directory.Documents,
+      path: ''
+    });
+
+    const fileListElement = document.getElementById('file-list');
+
+    if (result.files.length === 0) {
+      fileListElement.innerHTML = '<ion-item><ion-label>Keine Dateien gefunden</ion-label></ion-item>';
+      return;
+    }
+
+    result.files.forEach(file => {
+      const fileItem = document.createElement('ion-item');
+      fileItem.innerHTML = `<ion-label>${file}</ion-label>`;
+      fileListElement.appendChild(fileItem);
+    });
+  } catch (e) {
+    console.error('Unable to read dir', e);
+  }
+}
 </script>
 
 <style scoped>
